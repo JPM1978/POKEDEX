f4aa031b2cfe73adeb20092a677f259dc5b2e563
Jean Mayard <sanctuarygames30@gmail.com>
2022-11-26 23:04:52 -0500

    created index, show and images

--- server.js
+++ server.js
@@ -1,40 +1,30 @@
 // Dependencies
 const express = require('express');
+const pokemon = require('./models/pokemon.js');
 const app = express();
 const PORT = 3000;
-const mongoose = require('mongoose');
-const Tweet = require('./models/pokemon.js');
+const pokemonData = require('./models/pokemon.js');
 
-
-// Database configuration
-const DATABASE_URL =
-'mongodb+srv://jmayard:Mayard2022@cluster0.lra6jzm.mongodb.net/Pokedex?retryWrites=true&w=majority'
-const db = mongoose.connection;
-
-// Connect to MongoDB Atlas
-mongoose.connect(DATABASE_URL, {
-    useNewUrlParser: true,
-	useUnifiedTopology: true,
-});
 
 
 // INDEX
-app.get('/', (req, res) => {
-    res.render('index.ejs', { data: Pokemon });
+app.get('/pokemon', (req, res) => {
+    res.render('index.ejs', {pokemon: pokemonData});
     });
 
 // SHOW
-app.get('/:id', (req, res) => {
-    res.render('show.ejs', { data: Pokemon[req.params.id] });
+app.get('/pokemon/:id', (req, res) => {
+    const pokemonId = req.params.id;
+    const selectedPokemon = pokemonData.find(pokemon => pokemon.id == pokemonId);
+    res.render('show.ejs',{pokemon:selectedPokemon} );
     });
 
+// EDIT
+app.get('/pokemon/:id/edit', (req, res) => {
+    res.render('edit.ejs');
+})
 
 
-// Database Connection Error/Success
-// Define callback functions for various events
-db.on('error', (err) => console.log(err.message + ' is mongod not running?'));
-db.on('connected', () => console.log('mongo connected'));
-db.on('disconnected', () => console.log('mongo disconnected'));
 
 
 // Listener
