dcaca54faa0306533436017b59003175a239ae12
Jean Mayard <sanctuarygames30@gmail.com>
2022-11-28 09:46:58 -0500

    update wallpaper, text and add edit values

--- server.js
+++ server.js
@@ -17,6 +17,19 @@ app.get('/pokemon', (req, res) => {
     res.render('index.ejs', {pokemon: pokemonData});
     });
 
+    
+// DELETE ROUTE
+app.delete("/pokemon/:id", (req, res) => {
+    // get the id from params
+    const id = req.params.id
+    // delete the pokemon
+    pokemon.findByIdAndRemove(id, (err, pokemon) => {
+    // redirect user back to index page
+    res.redirect("/pokemon")
+        })
+      })
+
+
 // SHOW
 app.get('/pokemon/:id', (req, res) => {
     const pokemonId = req.params.id;
@@ -42,6 +55,8 @@ app.put('/pokemon/:id', (req, res) => {
     // console.log(req.body.name)
     pokemonData[pokemonIndex].name = req.body.name;
     pokemonData[pokemonIndex].stats.hp = req.body.hp;
+    pokemonData[pokemonIndex].stats.attack = req.body.attack;
+    pokemonData[pokemonIndex].stats.defense = req.body.defense;
     res.redirect('/pokemon');
 })
 
